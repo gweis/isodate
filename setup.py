d261f3091d606598295e419c7b7d90b5d1ced10b
Gerhard Weis <g.weis@griffith.edu.au>
2014-11-07 05:11:21 +1000

    add Python 3.4 to list of supported versions

--- setup.py
+++ setup.py
@@ -75,6 +75,7 @@ setup(name='isodate',
                    'Programming Language :: Python :: 2.7',
                    'Programming Language :: Python :: 3.2',
                    'Programming Language :: Python :: 3.3',
+                   'Programming Language :: Python :: 3.4',
                    'Programming Language :: Python :: Implementation :: PyPy',
                    'Topic :: Internet',
                    ('Topic :: Software Development :'
