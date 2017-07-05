1d32c9eaf6b218ace851e1edd86a5af832d62c20
Gerhard Weis <g.weis@griffith.edu.au>
2017-07-06 04:36:26 +1000

    version is 0.6 dev

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ def read(*rnames):
 
 
 setup(name='isodate',
-      version='0.7.0.dev',
+      version='0.6.0.dev',
       packages=['isodate', 'isodate.tests'],
       package_dir={'': 'src'},
 
