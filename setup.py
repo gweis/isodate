b4e3d2a3efeb4805dd3058242178c1d480beeb9f
Jose Eduardo <jose.eduardo.gd@gmail.com>
2019-07-19 16:23:18 +0100

    Add support for py37 and py38

--- setup.py
+++ setup.py
@@ -68,6 +68,8 @@ setup(name='isodate',
                    'Programming Language :: Python :: 3.4',
                    'Programming Language :: Python :: 3.5',
                    'Programming Language :: Python :: 3.6',
+                   'Programming Language :: Python :: 3.7',
+                   'Programming Language :: Python :: 3.8',
                    'Programming Language :: Python :: Implementation :: PyPy',
                    'Topic :: Internet',
                    ('Topic :: Software Development :'
