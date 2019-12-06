54f89863413ae1ac89c7a0d4c45f843ca8024235
Hugo <hugovk@users.noreply.github.com>
2019-12-06 23:49:15 +0200

    Drop support for EOL Python 3.4

--- setup.py
+++ setup.py
@@ -55,6 +55,7 @@ setup(name='isodate',
                         read('CHANGES.txt') +
                         read('TODO.txt')),
 
+      python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*',
       classifiers=['Development Status :: 4 - Beta',
                    # 'Environment :: Web Environment',
                    'Intended Audience :: Developers',
@@ -64,9 +65,9 @@ setup(name='isodate',
                    'Programming Language :: Python :: 2',
                    'Programming Language :: Python :: 2.7',
                    'Programming Language :: Python :: 3',
-                   'Programming Language :: Python :: 3.4',
                    'Programming Language :: Python :: 3.5',
                    'Programming Language :: Python :: 3.6',
+                   'Programming Language :: Python :: Implementation :: CPython',
                    'Programming Language :: Python :: Implementation :: PyPy',
                    'Topic :: Internet',
                    ('Topic :: Software Development :'
